119665bcc2ad25ee28487ca40ad642f8226dfb87
백관열 <djm030@naver.com>
2023-04-12 19:06:08 +0900

    setting swagger

--- config/urls.py
+++ config/urls.py
@@ -1,22 +1,44 @@
-"""
-URL configuration for config project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from django.conf import settings
+from rest_framework.permissions import AllowAny
+from rest_framework import routers, permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="폼 미쳤다 강의사이트 ",  # 타이틀
+        default_version="v1",  # 버전
+        description="프로젝트 API 문서",  # 설명
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="djm030@naver.com"),
+        license=openapi.License(name="백관열"),
+    ),
+    validators=["flex"],
+    public=True,
+    permission_classes=(AllowAny,),
+)
+
+
 
 urlpatterns = [
+    ## admin
     path('admin/', admin.site.urls),
+    ## swagger
+    path(
+        "swagger<str:format>/",
+        schema_view.without_ui(cache_timeout=0),
+        name="schema-json",
+    ),
+    path(
+        "swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    path(
+        "redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc-v1"
+    ),
+    ## api
 ]
